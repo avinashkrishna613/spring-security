0539e240095354cd770b6681dd1ac73618b2d3ec
akolap <avinash.kolaparthi@icims.com>
2026-03-01 01:54:54 +0530

    add optional for the repository methods

--- src/main/java/com/example/security/repository/TaskRepository.java
+++ src/main/java/com/example/security/repository/TaskRepository.java
@@ -1,11 +1,12 @@
 package com.example.security.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.security.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-     List<Task> findByUserId(Long userId);
+     Optional<List<Task>> findByUserId(Long userId);
 }
--- src/main/java/com/example/security/repository/UserRepository.java
+++ src/main/java/com/example/security/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.security.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
